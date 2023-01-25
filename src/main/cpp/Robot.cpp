c3106063cc17f78e03f109714fa62d32485c102b
SteelTalonsTerminal <81189397+SteelTalonsTerminal@users.noreply.github.com>
2023-01-25 10:24:22 -0500

    Added drivetrain control

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <frc/TimedRobot.h>
+#include <frc/XboxController.h>
+#include <frc/drive/DifferentialDrive.h>
+#include <frc/motorcontrol/MotorControllerGroup.h>
+#include <frc/motorcontrol/Spark.h>
+#include "ctre/Phoenix.h"
+
+
+
+/**
+ * Using the DifferentialDrive class.
+ * Runs the motors with split arcade steering and an Xbox controller.
+ */
+class Robot : public frc::TimedRobot {
+  WPI_TalonSRX m_BackleftMotor{12};
+  WPI_TalonSRX m_BackrightMotor{1};
+    WPI_TalonSRX m_FrontleftMotor{13};
+  WPI_TalonSRX m_FrontrightMotor{2};
+  frc::MotorControllerGroup m_left{m_BackleftMotor, m_FrontleftMotor};
+  frc::MotorControllerGroup m_right{m_BackrightMotor, m_FrontrightMotor};
+  frc::DifferentialDrive m_drive{m_left, m_right};
+  frc::XboxController m_driverController{0};
+
+
+ public:
+  void RobotInit() override {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+  
+    m_right.SetInverted(true);
+  }
+
+  void TeleopPeriodic() override {
+
+    m_drive.ArcadeDrive(m_driverController.GetLeftY(),
+                             -m_driverController.GetRightX());
+  }
+};
+
+#ifndef RUNNING_FRC_TESTS
+int main() {
+  return frc::StartRobot<Robot>();
+}
+#endif
