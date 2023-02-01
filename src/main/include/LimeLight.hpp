2224c0b02f353599f8b7cf8c97f593fcf5f2e72f
radioactive-link <dunsethsamiam@gmail.com>
2023-02-01 16:15:26 -0500

    Added limelight

---
+++ src/main/include/LimeLight.hpp
@@ -0,0 +1,16 @@
+#include "frc/smartdashboard/Smartdashboard.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableInstance.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableValue.h"
+#include "wpi/span.h"
+
+
+class LimeLight {
+public:
+    LimeLight();
+    void Update();
+
+private:
+
+};
