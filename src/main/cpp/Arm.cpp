5c8be6301946dee155cd2b7afb6f119cfc3ebb22
radioactive-link <dunsethsamiam@gmail.com>
2023-02-11 09:27:14 -0500

    Commented out jointhelper controller for now, supposed to only use one controller for two motors

--- src/main/cpp/Arm.cpp
+++ src/main/cpp/Arm.cpp
@@ -70,20 +70,20 @@ void Arm::CheckControllerState() {
   //make sure that if both buttons are pressed, arm doesn't try to move both ways
   if (armMovingForward && !armMovingBackward) {
     armJoint->Set(0.5);
-    armJointHelper->Set(-0.5);
+    //armJointHelper->Set(-0.5);
   }
   else if (!armMovingBackward) {//arm isn't currently trying to move back
     armJoint->Set(0.0);
-    armJointHelper->Set(0.0);
+    //armJointHelper->Set(0.0);
   }
 
   if (armMovingBackward && !armMovingForward) {
     armJoint->Set(-0.5);
-    armJointHelper->Set(0.5);
+    //armJointHelper->Set(0.5);
   }
   else if (!armMovingForward) {
     armJoint->Set(0.0);
-    armJointHelper->Set(0.0);
+    //armJointHelper->Set(0.0);
   }
   // ===== END IF "LOOPS" =====
  /* --=[ END ]=-- */
